5bbbd08cbbb1da45377622648b7147c2264351b5
adityam789 <adityam789@gmail.com>
2021-06-30 17:22:40 +0530

    sampler update

--- json_sample.py
+++ json_sample.py
@@ -23,7 +23,7 @@ def preprocessing_stage():
     if config.getboolean('vis', 'mean_plot'):
         vis_path = sep.join([config.get('vis', 'vis_path_folder1'),
                                      'mean_plot.png'])
-        d1= {'img_path': vis_path}
+        d1= {'img_path': vis_path[10:]}
         mean_dist.update(d1)
    
 
@@ -46,42 +46,42 @@ def modelling_stage():
     if config.getboolean('vis', 'model_performance'):
         mdl_prm = sep.join([config.get('vis', 'vis_path_folder2'),
                                      'model_performance.png'])
-        d1= {'img_path': mdl_prm}
+        d1= {'img_path': mdl_prm[10:]}
         model_performance.update(d1)
         
     model_predection = {}
     if config.getboolean('vis', 'model_predection'):
         mdl_pred = sep.join([config.get('vis', 'vis_path_folder2'),
                                      'model_predection.png'])
-        d1= {'img_path': mdl_pred}
+        d1= {'img_path': mdl_pred[10:]}
         model_predection.update(d1)
 
     model_with_drift_detection_performance = {}
     if config.getboolean('vis', 'model_with_drift_detection_performance'):
         mdl_pred = sep.join([config.get('vis', 'vis_path_folder2'),
                                      'model_performance_CD.png'])
-        d1= {'img_path': mdl_pred}
+        d1= {'img_path': mdl_pred[10:]}
         model_with_drift_detection_performance.update(d1)
 
     model_with_drift_detection_MAE = {}
     if config.getboolean('vis', 'model_with_drift_detection_MAE'):
         mdl_pred = sep.join([config.get('vis', 'vis_path_folder2'),
                                      'MAE.png'])
-        d1= {'img_path': mdl_pred}
+        d1= {'img_path': mdl_pred[10:]}
         model_with_drift_detection_MAE.update(d1)
 
     models_comparison = {}
     if config.getboolean('vis', 'models_comparison'):
         mdl_pred = sep.join([config.get('vis', 'vis_path_folder2'),
                                      'performance_comparison.png'])
-        d1= {'img_path': mdl_pred}
+        d1= {'img_path': mdl_pred[10:]}
         models_comparison.update(d1)
 
     models_MAE_comparison = {}
     if config.getboolean('vis', 'models_MAE_comparison'):
         mdl_pred = sep.join([config.get('vis', 'vis_path_folder2'),
                                      'performance_difference_comparison.png'])
-        d1= {'img_path': mdl_pred}
+        d1= {'img_path': mdl_pred[10:]}
         models_MAE_comparison.update(d1)
 
     json_report["Modelling"] = {
